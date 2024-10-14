d19819246b4ec8f4ac482d71faae53afb1d52f35
Samuel do nascimento ramos <hmsrp.ramos7@gmail.com>
2024-10-14 16:52:22 -0300

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,5 @@
+let anos = parseInt(prompt("digite a idade da pessoa"))
+let dias = parseInt(prompt("digite os dias"))
+let meses = parseInt (prompt("digite os meses"))
+let resultado = anos * 365 + meses * 30 + dias
+alert ("sua idade expressa em anos é: " + resultado )
